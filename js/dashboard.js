6dfe2986dfd9e741f9ce1d387296ff9069b2b513
PrinceNiyonshuti <npprince47@gmail.com>
2022-02-25 15:15:21 +0200

    task(store article): managed to store article to firestore

--- js/dashboard.js
+++ js/dashboard.js
@@ -12,3 +12,40 @@ function logOut() {
 	localStorage.clear();
 	window.location.href = "./../login.html";
 }
+
+/*
+ @role send query 
+*/
+function saveArticle() {
+	const cover = document.getElementById("cover").value;
+	const title = document.getElementById("title").value;
+	const author = document.getElementById("author").value;
+	const slug = document.getElementById("slug").value;
+	const description = document.getElementById("description").value;
+	// console.log(title, author, slug, description, cover);
+	db.collection("articles")
+		.doc()
+		.set({
+			cover,
+			title,
+			author,
+			slug,
+			description,
+			created_at: new Date(),
+		})
+		.then(() => {
+			swal({
+				title: "Article Saved",
+				icon: "success",
+				timer: 2000,
+			});
+		})
+		.catch((error) => {
+			swal({
+				title: "Error",
+				text: `Something went wrong`,
+				icon: "error",
+				timer: 2000,
+			});
+		});
+}
